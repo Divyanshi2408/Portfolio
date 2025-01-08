d4bb35066531fbfa0bcdf70bb3cd633b2a355970
Divyanshi <85859444+Divyanshi2408@users.noreply.github.com>
2025-01-08 23:15:51 +0530

    update

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -3,6 +3,9 @@ import SnakeGame from "../components/SnakeGame"; // Import SnakeGame component
 import "./HomePage.css"; // Import styles
 import d1 from "../Assets/images/d1.png"; // Replace with your actual image path
 import Footer from "../components/Footer";
+import AboutPage from "./AboutPage";
+import Projects from "./Projects";
+import Contact from "./Contact";
 
 const HomePage = () => {
   const [showGame, setShowGame] = useState(true);
@@ -27,7 +30,7 @@ const HomePage = () => {
   }, []);
 
   return (
-    
+    <div>
     <div className="home-container">
       <div className="transparent-layer"></div>
       <div className="intro-section">
@@ -72,7 +75,13 @@ const HomePage = () => {
         )}
       </div>
       </div>
-   
+      <h1 className="section">About Me</h1>
+      <AboutPage/>
+      <h1 className="section">Projects</h1>
+      <Projects/>
+      <h1 className="section">Contact</h1>
+      <Contact/>
+      </div>
    
   );
   
